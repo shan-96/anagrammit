d6fd30add659f3346607debcbf93192e41ab57f0
adam.bachman <adam.bachman@e9d24f8c-8f23-0410-9448-915246385cd9>
2006-12-01 19:07:36 +0000

    Improvement.

--- trunk/anagrammer.py
+++ trunk/anagrammer.py
@@ -1,13 +1,20 @@
 ##-----  Here begins the program  -----##
 # An anagram generator using a recursive function.
 # Created by: Adam Bachman
-# Date: 2005
+# Date: October 2005
+# Update: Oct-13-2005
+#   -removed regular expressions from the lexicon builder.
 
 from string import ascii_lowercase
-import re
+from time import time
 import psyco
+import sys
+ 
 psyco.full()
+#psyco.log()
 #psyco.profile()
+
+createCount = 0
 
 #####################  INPUT FUNCTION  #######################
 # take the original input, and remove some letters from it.
@@ -15,22 +22,22 @@ def stringRemove(orig, remove):
     for let in remove:
         orig = orig.replace(let,'',1)
     return orig
-#
+
 #####################  LEXICON FUNCTION  #######################        
 def createLexicon(lexi, input):
+    global createCount 
+    createCount += 1
     '''generate lexicon'''
-    # foreign letter checker
-    # a RegularExpression-ized string of the letters NOT in the input string.
-    foreign_re = re.compile('['+''.join([x for x in ascii_lowercase if input.count(x) == 0])+']')
-    #returns foreign checked dictionary.
-    temp_dict = [x for x in lexi if not foreign_re.search(x)]
-
+    #foreign = stringRemove(ascii_lowercase, input) # string of all letters not in the input
     new_dict = []
-    for word in temp_dict:
+    for word in lexi:
         bad = False
         for letter in word:
-            # if it doesn't have too many of any particular letter...
-            if word.count(letter) > input.count(letter):
+            # if it doesn't have too many of any particular letter and doesn't have any foreign letters...
+            if letter not in input:
+                bad = True
+                break
+            elif word.count(letter) > input.count(letter):
                 bad = True
                 break
         # add it to the original lexicon
@@ -43,63 +50,64 @@ def createLexicon(lexi, input):
 ## an anagram.  That means if a particular anagram has eight words, our max
 ## recursion depth is eight.
 def MainLoop(lexi, inpt, rslt, temp_rslt=''):
-    while len(lexi) > 0:
-        #append the first word in the lexicon to temp_rslt
-        temp_rslt = (temp_rslt+' '+lexi[0]).strip()
-        #update the input string
-        new_inpt = stringRemove(inpt, lexi[0])
+    count = -1
+    for next_word in lexi:
+        count += 1 # counter to remember where in the list we are.
         
+        #append the first word in the lexicon to temp_rslt
+        temp_rslt += ' '+next_word
+        
+        #update the input string
+        new_inpt = stringRemove(inpt, next_word)
         if len(new_inpt) == 0:
             # Branch A
             # Empty new input, full old lexicon.  We've got a winner!  
-            # Print it and add it to the results.
-            print temp_rslt
+
+#            print temp_rslt
+#            sys.exit(1)
+            
+            rslt[0] += 1
+            if rslt[0] % 100 == 0:
+                print rslt[0]
             rslt.append(temp_rslt)
             #get rid of the word most recently used from old lexicon and temp_rslt.
-            temp_rslt = temp_rslt[:-len(lexi.pop(0))-1]
+            temp_rslt = temp_rslt[:-len(next_word)-1]
         else:
             # Full new input, full old lexicon.  Make a new lexicon.
-            temp_lexi = createLexicon(lexi, new_inpt)
+            temp_lexi = createLexicon(lexi[count:], new_inpt)
             if len(temp_lexi) == 0:
                 # Branch B
                 # Full new input, empty new lexicon.                 
-                #delete temp_lexi, repeat from the while.
-                del temp_lexi
                 #get rid of the word most recently used from old lexicon and temp_rslt.
-                temp_rslt = temp_rslt[:-len(lexi.pop(0))-1]
+                temp_rslt = temp_rslt[:-len(next_word)-1]
             else:
                 # Branch C
                 # Full new input, full new lexicon.  Go down one level.
                 MainLoop(temp_lexi, new_inpt, rslt, temp_rslt)
                 #get rid of the word most recently used from old lexicon and temp_rslt.
-                temp_rslt = temp_rslt[:-len(lexi.pop(0))-1]
-    return
+                temp_rslt = temp_rslt[:-len(next_word)-1]
+                
 
-
-# Testing run or real run?
-test = True
-
-if test:
-#    inpt = "urinepoopanus"
-#    inpt = "into"
-    inpt = raw_input("Enter the phrase to be anagrammed: ")
-    inpt = ''.join([l for l in inpt.lower() if ascii_lowercase.count(l)>0])
-    print inpt
-    dictionary = createLexicon(['inn','into','to','i','n','t','o'],inpt)
-    print dictionary
+if __name__=="__main__":    
+    inpt = "testmeplease"
+    #    inpt = "into"
+    #res_file = file('onebig_results','w')
+    #inpt = raw_input("Enter the phrase to be anagrammed: ")
+    #inpt = ''.join([l for l in inpt.lower() if l.isalpha()])
+    s = time()    
+    result = [0]
+    dictionary = createLexicon([x.strip() for x in open('dictionary.txt')],inpt)
+    MainLoop(dictionary,inpt,result)
+    f = time()
+#    for x in result:
+#        res_file.write("%s\n"%x)
+    print "%f seconds used."%(f-s)
     print "DONE TESTING"
-
-else:
-    inpt = raw_input("Enter the phrase to be anagrammed: ")
-    inpt = ''.join([l for l in inpt.lower() if ascii_lowercase.count(l)>0])
-    LEXICON = createLexicon([x.strip() for x in open('dictionary.txt')], inpt)
-    RESULTS = []
-    if len(LEXICON) < 1:
-        print "No lexicon, what a terrible input string!"
-    else:
-        print "GOING!"
-        # Enter the recursive anagram finding function.
-        MainLoop(LEXICON, inpt, RESULTS)
-        # Once it's done, RESULTS contains all our results
-        print "DONE!"
-        print len(RESULTS), " anagrams found"
+    print "%i results found"%result[0]
+    #for x in result[0:10]: print x
+    #for x in result[-10:]: print x      
+#    res_file.close()
+    
+    print "createLexicon was run",createCount,"times."
+                
+            
